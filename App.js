c9b5cfe22418db7e5e618bcda605367e151e6e59
Allandino <allandhino.widyaputra@salt.co.id>
2019-11-12 18:49:49 +0700

    # init project

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import AppNavigator from './src/Navigator';
+
+const App: () => React$Node = () => {
+  return (
+    <AppNavigator/>
+  );
+};
+
+export default App;
---
+++ src/Navigator/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createStackNavigator } from 'react-navigation-stack';
+import {
+    createAppContainer,
+} from "react-navigation";
+import HomeScreen from '../Screens/HomeScreen';
+import LoginScreen from '../Screens/LoginScreen';
+
+const AppNavigator = createStackNavigator(
+    {
+        Home: {
+            screen: HomeScreen,
+        },
+        Login: {
+            screen: LoginScreen,
+        },
+    },
+    {
+        initialRouteName: "Login",
+    }
+);
+
+
+const Navigator = createAppContainer(AppNavigator);
+
+export default Navigator;
